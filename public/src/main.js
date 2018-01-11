a1f64a5d45e59d311a4132fbade06aa177ad50f0
Paul Peavyhouse <pv@swooby.com>
2018-01-10 17:13:58 -0800

    Branch to attempt to start over as a Vue Progressive Web App

---
+++ public/src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import * as firebase from 'firebase'
+import router from './router'
+import { store } from './store'
+import AlertCmp from './components/Shared/Alert.vue'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+Vue.component('app-alert', AlertCmp)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App },
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDfKA77M6vyodG8_BprKviSgNtB0zLoVDU',
+      authDomain: 'trade-citizen.firebaseapp.com',
+      databaseURL: 'https://trade-citizen.firebaseio.com',
+      projectId: 'trade-citizen',
+      storageBucket: 'trade-citizen.appspot.com',
+      messagingSenderId: '676462601414'
+    })
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user)
+      }
+    })
+  }
+})
---
+++ public/src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Profile from '@/components/User/Profile'
+import Signup from '@/components/User/Signup'
+import Signin from '@/components/User/Signin'
+import AuthGuard from './auth-guard'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      beforeEnter: AuthGuard
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    }
+  ],
+  mode: 'history'
+})
