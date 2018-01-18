328747af9981b7f261e34afc284a123b30c0b924
Paul Peavyhouse <pv@swooby.com>
2018-01-17 18:09:14 -0800

    Getting realtime firestore updates from server

--- public/src/store/tradeinfo/index.js
+++ public/src/store/tradeinfo/index.js
@@ -2,33 +2,64 @@ import * as firebase from 'firebase'
 
 export default {
   state: {
-    commodityCategories: [
+    commodityCategoriesList: [
     ],
-    commodities: [
+    commoditiesList: [
     ],
-    stations: [
+    stationsList: [
     ]
   },
   mutations: {
     setCommodityCategories (state, payload) {
-      state.commodityCategories = payload
+      state.commodityCategoriesList = payload.sort((a, b) => {
+        let aName = a.name.toLowerCase()
+        let bName = b.name.toLowerCase()
+        if (aName < bName) {
+          return -1
+        }
+        if (aName > bName) {
+          return 1
+        }
+        return 0
+      })
     },
     setCommodities (state, payload) {
-      state.commodities = payload
+      state.commoditiesList = payload.sort((a, b) => {
+        let aName = a.name.toLowerCase()
+        let bName = b.name.toLowerCase()
+        if (aName < bName) {
+          return -1
+        }
+        if (aName > bName) {
+          return 1
+        }
+        return 0
+      })
     },
     setStations (state, payload) {
-      state.stations = payload
+      state.stationsList = payload.sort((a, b) => {
+        let aName = a.name.toLowerCase()
+        let bName = b.name.toLowerCase()
+        if (aName < bName) {
+          return -1
+        }
+        if (aName > bName) {
+          return 1
+        }
+        return 0
+      })
     }
   },
   actions: {
     fetchTradeinfo ({commit}) {
-      commit('setLoading', true)
+      // commit('setLoading', true)
+
       firebase.firestore().collection('itemCategories')
-        .get()
-        .then((querySnapshot) => {
+        .onSnapshot((querySnapshot) => {
           console.log('got itemCategories')
           const commodityCategories = []
           querySnapshot.forEach((doc) => {
+            // console.log(doc)
             let data = doc.data()
             let commodityCategory = {
               id: doc.id,
@@ -38,83 +69,61 @@ export default {
             commodityCategories.push(commodityCategory)
           })
           commit('setCommodityCategories', commodityCategories)
-
-          firebase.firestore().collection('itemTypes')
-            .get()
-            .then((querySnapshot) => {
-              console.log('got itemTypes')
-              const commodities = []
-              querySnapshot.forEach((doc) => {
-                // console.log(doc)
-                let data = doc.data()
-                let commodity = {
-                  id: doc.id,
-                  name: data.name
-                }
-                // console.log('commodity.name:' + commodity.name)
-                commodities.push(commodity)
-              })
-              commit('setCommodities', commodities)
-
-              firebase.firestore().collection('stations')
-                .get()
-                .then((querySnapshot) => {
-                  console.log('got stations')
-                  const stations = []
-                  querySnapshot.forEach((doc) => {
-                    // console.log(doc)
-                    let data = doc.data()
-                    let station = {
-                      id: doc.id,
-                      anchor: data.anchor,
-                      name: data.name,
-                      stationType: data.type
-                    }
-                    // console.log('station.name:' + station.name)
-                    stations.push(station)
-                  })
-                  commit('setStations', stations)
-
-                  commit('setLoading', false)
-                })
-                .catch((error) => {
-                  console.error('Error getting stations', error)
-                  commit('setLoading', false)
-                })
-            })
-            .catch((error) => {
-              console.error('Error getting itemTypes', error)
-              commit('setLoading', false)
-            })
-        })
-        .catch((error) => {
+        }, (error) => {
           console.error('Error getting itemCategories', error)
-          commit('setLoading', false)
+          // commit('setLoading', false)
+        })
+
+      firebase.firestore().collection('itemTypes')
+        .onSnapshot((querySnapshot) => {
+          console.log('got itemTypes')
+          const commodities = []
+          querySnapshot.forEach((doc) => {
+            // console.log(doc)
+            let data = doc.data()
+            let commodity = {
+              id: doc.id,
+              name: data.name
+            }
+            // console.log('commodity.name:' + commodity.name)
+            commodities.push(commodity)
+          })
+          commit('setCommodities', commodities)
+        }, (error) => {
+          console.error('Error getting itemTypes', error)
+          // commit('setLoading', false)
+        })
+
+      firebase.firestore().collection('stations')
+        .onSnapshot((querySnapshot) => {
+          console.log('got stations')
+          const stations = []
+          querySnapshot.forEach((doc) => {
+            // console.log(doc)
+            let data = doc.data()
+            let station = {
+              id: doc.id,
+              anchor: data.anchor,
+              name: data.name,
+              stationType: data.type
+            }
+            // console.log('station.name:' + station.name)
+            stations.push(station)
+          })
+          commit('setStations', stations)
+          // commit('setLoading', false)
+        }, (error) => {
+          console.error('Error getting stations', error)
+          // commit('setLoading', false)
         })
     }
   },
   getters: {
     commodities (state) {
-      return state.commodities.sort((commodityA, commodityB) => {
-        if (commodityA.name < commodityB.name) {
-          return -1
-        }
-        if (commodityA.name > commodityB.name) {
-          return 1
-        }
-        return 0
-      })
+      return state.commoditiesList
     },
     stations (state) {
-      return state.stations.sort((stationA, stationB) => {
-        if (stationA.name < stationB.name) {
-          return -1
-        }
-        if (stationA.name > stationB.name) {
-          return 1
-        }
-        return 0
-      })
+      return state.stationsList
     }
   }
 }
