f2eada38583b88b24153aafc5b52706f7dcfe1c3
Paul Peavyhouse <pv@swooby.com>
2018-03-12 16:36:55 -0700

    SSF tweak and app reload when update detected

--- hosting/build/service-worker-prod.js
+++ hosting/build/service-worker-prod.js
@@ -35,7 +35,8 @@
                     // fresh content will have been added to the cache.
                     // It's the perfect time to display a "New content is
                     // available; please refresh." message in the page's interface.
-                    console.log('New content is available; please refresh.');
+                    console.log('New content is available; reloading...');
+                    window.location.reload();
                     break;
 
                   case 'redundant':
