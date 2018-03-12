25edbef712f42318cffd207dbcdf76d24cf7c74f
Paul Peavyhouse <pv@swooby.com>
2018-03-12 15:50:17 -0700

    functions wildcard deployment root

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,8 @@ const functions = require('firebase-functions');
 // https://firebase.google.com/docs/firestore/extend-with-functions
 // https://github.com/firebase/functions-samples
 
-const ROOT = '/deployments/test/'
-
 exports.onPriceCreated = functions.firestore
-  .document(ROOT + 'stations/{stationId}/prices/{priceId}')
+  .document('/deployments/{deploymentId}/stations/{stationId}/prices/{priceId}')
   .onCreate(onPriceCreated)
   // TODO:(pv) .onUpdate, .onDelete, .onWrite?
   ;
