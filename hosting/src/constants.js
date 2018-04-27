e81ac0621914b7228696f6d3761202a4d7f8745d
Paul Peavyhouse <pv@swooby.com>
2018-04-27 13:04:25 -0700

    Adding known Ship SCU constants

---
+++ hosting/src/constants.js
@@ -0,0 +1,65 @@
+
+//
+// Official:
+//  https://robertsspaceindustries.com/ship-matrix
+// NOTE:(pv) Many ships, including some in-game (ex: Prospector), don't specify Cargo Capacity.
+//
+// 3rd Party (only that closely match the official Ship Matrix):
+//  https://starcitizen.tools/Ships
+//  https://docs.google.com/spreadsheets/d/19WsIZRWSstfzKwUHX9ha0WS_3skVWUhOpuF9j3-CPKc/edit#gid=1993453954
+//
+// NOTE:(pv) Only the latest flyable ships should be enabled.
+//  Commented out ships are not confirmed in-game.
+//  Any commented out ship should be confirmed before uncommenting.
+//
+exports.SHIP_CARGOS = {
+  'Cyclone': { scu: 1 },
+  'Dragonfly': { scu: 1 },
+  // '100i': { scu: 2 },
+  // '125a': { scu: 2 },
+  '300i': { scu: 2 },
+  '315p': { scu: 2 },
+  '325a': { scu: 2 },
+  'F7C Hornet': { scu: 2 },
+  'MPUV Cargo': { scu: 2 },
+  'Reliant Kore': { scu: 4 },
+  'Ursa Rover': { scu: 4 },
+  // '135c': { scu: 6 },
+  'Aurora CL': { scu: 6 },
+  'Mustang Alpha': { scu: 6 },
+  'Avenger Titan': { scu: 8 },
+  // 'Vulcan': { scu: 12 },
+  // '600i Tourer': { scu: 16 },
+  // 'Cutlass Red': { scu: 16 },
+  // 'Cutlass Blue': { scu: 25 },
+  // 'Freelancer DUR': { scu: 28 },
+  // 'Freelancer MIS': { scu: 28 },
+  // '600i Explorer': { scu: 40 },
+  // 'Hammerhead': { scu: 40 },
+  'Cutlass Black': { scu: 46 },
+  // 'Hull A': { scu: 48 },
+  'Freelancer': { scu: 66 },
+  'Constellation Andromeda': { scu: 96 },
+  'Constellation Aquila': { scu: 96 },
+  // 'Freelancer MAX': { scu: 122 },
+  'Reclaimer': { scu: 180 },
+  // 'Polaris': { scu: 216 },
+  // 'Crucible': { scu: 230 },
+  'Starfarer': { scu: 295 },
+  'Starfarer Gemini': { scu: 295 },
+  // 'Genesis Starliner': { scu: 300 },
+  // 'Hull B': { scu: 384 },
+  // 'Orion': { scu: 384 },
+  // 'Endeavor': { scu: 500 },
+  'Caterpillar': { scu: 576 }
+  // 'Pioneer': { scu: 600 },
+  // 'Idris M': { scu: 831 },
+  // 'Idris P': { scu: 995 },
+  // 'Carrack': { scu: 1000 },
+  // '890 Jump': { scu: 1600 },
+  // 'Merchantman': { scu: 3584 },
+  // 'Hull C': { scu: 4608 },
+  // 'Javelin': { scu: 5400 },
+  // 'Hull D': { scu: 20736 },
+  // 'Hull E': { scu: 98304 }
+}
